acd98a42d23c733869cb96e2363fa295c10c4b52
Xango2030 <evertoncordeiro@icloud.com>
2023-05-15 10:49:54 -0400

    correção de layout

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,4 +1,3 @@
-import { Icon } from '@iconify/react'
 import '../style/global.css'
 
 import {
@@ -6,36 +5,37 @@ import {
     Row,
     Col,
 } from 'reactstrap'
+import { Icon } from '@iconify/react'
 
 
 export default function Home() {
     return (
-        <Container className='container-size d-flex justify-content-center align-items-center'>
-            <div className="d-flex justify-content-between gap-5">
-                <div>
-                    <h1 className="title-home">
-                        Olá ! Meu nome é
-                        <br />
-                        <span className='gradient'>Éverton Cordeiro</span>
-                        <br />
-                        e sou um
-                        <br />
-                        <span className='gradient'>Dev Front-end</span>
-                    </h1>
-                    <div className='d-flex justify-content-center gap-5'>
-                        <Icon icon="bi:github"    style={{fontSize: '50'}} />
-                        <Icon icon="mdi:linkedin" style={{fontSize: '50'}} />
-                        <Icon icon="tabler:mail"  style={{fontSize: '50'}} />
+        <Container xl='12' fluid>
+        <Row> 
+          <Col xl="8" md="12" >
+            <div>
+              <h1>
+                Olá ! Me chamo
+                <br />
+                <span className='gradient'>Éverton Cordeiro</span>
+                <br />
+                e sou um
+                <br />
+                <span className='gradient'>Dev Front-end</span>
+              </h1>
 
-                    </div>
-                </div>
+              <div className='d-flex justify-content-center gap-5'>
+                <Icon icon="bi:github" style={{ fontSize: '50' }} />
+                <Icon icon="mdi:linkedin" style={{ fontSize: '50' }} />
+                <Icon icon="tabler:mail" style={{ fontSize: '50' }} />
+              </div>
 
-                <div>
-                    <img
-                        alt="memoji Olá"  src="https://raw.githubusercontent.com/EvertonCordeiro1994/portfolioReactStrap/main/src/components/img/ola.png"       />
-                </div>
             </div>
-
-        </Container>
+          </Col>
+          <Col lg="4" md="12" >
+            <img alt="memoji Olá" src="https://raw.githubusercontent.com/EvertonCordeiro1994/portfolioReactStrap/main/src/components/img/ola.png" />
+          </Col>
+        </Row>
+      </Container>
     )
 }
