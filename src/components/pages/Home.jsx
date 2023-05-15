8eefc1b8fccc2d18969fe8cd06cbb8adf5949987
Xango2030 <evertoncordeiro@icloud.com>
2023-05-15 18:02:00 -0400

    melhoria no layout da página home, melhoria no código da página home e exclusão de algumas classes no arquivo global.css

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -10,32 +10,32 @@ import { Icon } from '@iconify/react'
 
 export default function Home() {
     return (
-        <Container xl='12' fluid>
-        <Row> 
-          <Col xl="8" md="12" >
-            <div>
-              <h1>
-                Olá ! Me chamo
-                <br />
-                <span className='gradient'>Éverton Cordeiro</span>
-                <br />
-                e sou um
-                <br />
-                <span className='gradient'>Dev Front-end</span>
-              </h1>
+        <Container xl='12' fluid className="mt-6 mb-5">
+            <Row>
+                <Col xl="8" md="8" >
+                    <div>
+                        <h1 className='text-center'>
+                            Olá ! Me chamo
+                            <br />
+                            <span className='gradient'>Éverton Cordeiro</span>
+                            <br />
+                            e sou um
+                            <br />
+                            <span className='gradient'>Dev Front-end</span>
+                        </h1>
 
-              <div className='d-flex justify-content-center gap-5'>
-                <Icon icon="bi:github" style={{ fontSize: '50' }} />
-                <Icon icon="mdi:linkedin" style={{ fontSize: '50' }} />
-                <Icon icon="tabler:mail" style={{ fontSize: '50' }} />
-              </div>
+                        <div className='d-flex justify-content-center gap-5'>
+                            <Icon icon="bi:github" style={{ fontSize: '50' }} />
+                            <Icon icon="mdi:linkedin" style={{ fontSize: '50' }} />
+                            <Icon icon="tabler:mail" style={{ fontSize: '50' }} />
+                        </div>
 
-            </div>
-          </Col>
-          <Col lg="4" md="12" >
-            <img alt="memoji Olá" src="https://raw.githubusercontent.com/EvertonCordeiro1994/portfolioReactStrap/main/src/components/img/ola.png" />
-          </Col>
-        </Row>
-      </Container>
+                    </div>
+                </Col>
+                <Col lg="4" md="4" >
+                    <img alt="memoji Olá" src="https://raw.githubusercontent.com/EvertonCordeiro1994/portfolioReactStrap/main/src/components/img/ola.png" />
+                </Col>
+            </Row>
+        </Container>
     )
 }
