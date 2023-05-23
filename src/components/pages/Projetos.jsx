620d9190bc50f47228bd21279b50334a16a2520e
Xango2030 <evertoncordeiro@icloud.com>
2023-05-23 16:45:04 -0400

    adição de alguns textos

--- src/components/pages/Projetos.jsx
+++ src/components/pages/Projetos.jsx
@@ -15,19 +15,16 @@ export default function Projetos() {
     return (
         <Container>
             <Row>
-                <Col  className="text-center ">
+                <Col  className="text-center mb-5 ">
                     <h1 className="gradient">Projetos</h1>
-                    <p>
-                        Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                        Nisi, eligendi. Magnam repudiandae blanditiis voluptate modi
-                        molestias asperiores rerum corrupti numquam, ipsum odio possimus
-                        ducimus eligendi quidem corporis iure dolorem dolore?
+                    <p className="mb-5 ">
+                        Aqui é onde eu compartilho alguns dos meus projetos mais empolgantes e recentes. Eu adoro desafios criativos e estou animado para mostrar a você o que tenho feito!
                     </p>
                 </Col>
             </Row>
             <Row className=" mb-10">
                 <Col className="d-flex justify-content-center mb-4">
-                    <Card className="card" style= {{ width: '18rem' }} >
+                    <Card className="card" style= {{ width: '22rem' }} >
                         <img alt="Sample" src="https://raw.githubusercontent.com/EvertonCordeiro1994/portfolioReactStrap/main/src/components/img/calc.png" />
                         <CardBody className="Card-info">
                             <CardTitle tag="h5">
@@ -52,7 +49,7 @@ export default function Projetos() {
                     </Card>
                 </Col>
                 <Col className="d-flex justify-content-center mb-4">
-                    <Card style={{ width: '18rem' }}>
+                    <Card style={{ width: '22rem' }}>
                         <img alt="Sample" src="https://raw.githubusercontent.com/EvertonCordeiro1994/portfolioReactStrap/main/src/components/img/Landingpage.png" />
                         <CardBody>
                             <CardTitle tag="h5">
@@ -65,7 +62,7 @@ export default function Projetos() {
                                 <Icon icon='la:tools' /> HTML, CSS e JS
                             </CardSubtitle>
                             <CardText>
-                                Some quick example text to build on the card title and make up the bulk of the card‘s content.
+                                A landing page é uma página web projetada para promover um produto, serviço ou evento específico. Este projeto inclui uma landing page responsiva e estilizada usando apenas HTML e CSS puro. 
                             </CardText>
                             <CardLink href="https://github.com/EvertonCordeiro1994/ladingpageresponsiva">
                                 < Icon icon='ri:github-line' /> Repositório
@@ -77,7 +74,7 @@ export default function Projetos() {
                     </Card>
                 </Col>
                 <Col className="d-flex justify-content-center mb-4">
-                    <Card style={{ width: '18rem' }}>
+                    <Card style={{ width: '22rem' }}>
                         <img alt="Sample" src="https://raw.githubusercontent.com/EvertonCordeiro1994/portfolioReactStrap/main/src/components/img/to-do-list.png" />
                         <CardBody>
                             <CardTitle tag="h5">
@@ -90,7 +87,7 @@ export default function Projetos() {
                                 <Icon icon='la:tools' /> REACT JS e CSS
                             </CardSubtitle>
                             <CardText>
-                                Some quick example text to build on the card title and make up the bulk of the card‘s content.
+                                O To Do List é um aplicativo que permite aos usuários gerenciar suas tarefas diárias. Este projeto utiliza o framework React.js para criar uma interface interativa e responsiva, permitindo que os usuários adicionem, removam e atualizem tarefas. O CSS é utilizado para estilizar o aplicativo e fornecer uma experiência agradável aos usuários.
                             </CardText>
                             <CardLink href="https://github.com/EvertonCordeiro1994/todolistreactjs">
                                 < Icon icon='ri:github-line' /> Repositório
